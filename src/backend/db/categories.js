4b8f4f2858bba4ade2b89f5e716d6fade2028ff1
Bijaylaxmi Behera <72284560+bijaylaxmibehera@users.noreply.github.com>
2024-03-22 19:13:21 +0530

    database: add product category

--- src/backend/db/categories.js
+++ src/backend/db/categories.js
@@ -1,4 +1,3 @@
-import { v4 as uuid } from "uuid";
 
 /**
  * Category Database can be added here.
@@ -7,21 +6,33 @@ import { v4 as uuid } from "uuid";
 
 export const categories = [
   {
-    _id: uuid(),
-    categoryName: "fiction",
-    description:
-      "literature in the form of prose, especially novels, that describes imaginary events and people",
+    _id:"57a85a1c-2102-4881-8757-1226e0a1afea",
+    categoryName: "earrings",
+    categoryImageUrl:"https://res.cloudinary.com/bijaylaxmibehera/image/upload/v1711079791/jwellery/earrings.jpg"
   },
   {
-    _id: uuid(),
-    categoryName: "non-fiction",
-    description:
-      "Non-fiction is writing that gives information or describes real events, rather than telling a story.",
+    _id:"e6f5d326-2b8c-4f9c-ba78-8ffd9193bbda",
+    categoryName: "rings",
+    categoryImageUrl:"https://res.cloudinary.com/bijaylaxmibehera/image/upload/v1711079911/jwellery/rings.jpg"
   },
   {
-    _id: uuid(),
-    categoryName: "horror",
-    description:
-      "Meant to cause discomfort and fear for both the character and readers, horror writers often make use of supernatural and paranormal elements in morbid stories that are sometimes a little too realistic.",
+    _id:"56e26c76-4ca0-468d-a7cc-b14e35fe627a",
+    categoryName: "necklaces",
+    categoryImageUrl:"https://res.cloudinary.com/bijaylaxmibehera/image/upload/v1711080045/jwellery/necklaces.jpg"
+  },
+  {
+    _id:"76f6328f-996e-4c69-af42-e89c9e755b04",
+    categoryName: "bangles",
+    categoryImageUrl:"https://res.cloudinary.com/bijaylaxmibehera/image/upload/v1711080257/jwellery/bangles.jpg"
+  },
+  {
+    _id:"4e58a5d1-25bb-4b55-bde8-fd32d315d68f",
+    categoryName: "hair accessories",
+    categoryImageUrl:"https://res.cloudinary.com/bijaylaxmibehera/image/upload/v1711080434/jwellery/hair-accessories.jpg"
+  },
+  {
+    _id:"e7cd3807-8a2f-41f3-a174-3567a5c06502",
+    categoryName: "anklet",
+    categoryImageUrl:"https://res.cloudinary.com/bijaylaxmibehera/image/upload/v1711080593/jwellery/anklets.jpg"
   },
 ];
